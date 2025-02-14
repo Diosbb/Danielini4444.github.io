b4541a72f779407447916e6c2f0fafec81b67b7c
Danielini4444 <l22240014@smartin.tecnm.mx>
2025-02-14 00:28:04 -0600

    Add files via upload

---
+++ autobiografia.js
@@ -0,0 +1,36 @@
+function nombre() {
+	alert('Daniel Díaz Rojas')
+}
+function grupo() {
+	alert('6A ISC')
+}
+function artifav() {
+	alert('NSQK')
+}
+function persoamada() {
+	alert('Yo mero, ni modo que quien')
+}
+function lover() {
+	alert('Carolina Pinto Negrete')
+}
+function edad() {
+	alert('21 añitos sin bañarme JAJAJAJA')
+}
+function datocurioso() {
+	alert('Pienso en con quién estás cuando no estás conmigo\nOdio que mis planes no sean tu camino\nY haberlo negado por estar contigo\n¿Y quién me va a curar el corazón partío?')
+}
+function fotouno(){
+	alert('Esta foto es de mis tres años, aunque suene raro, fui catolico mitad de mi vida')
+}
+function fotodos(){
+	alert('Esta foto es de mis favoritas, fue cuando tenia poco tiempo de empezar en el TKD')
+}
+function fototres(){
+	alert('Esta foto es sin duda mi favorita de todas, la historia detras es linda, y la foto es sencillamente perfecta')
+}
+function fotocuatro(){
+	alert('Esta foto siento que representa mis vibes actuales y mi forma de vestir actualmente')
+}
+function fotocinco(){
+	alert('Esta fue mi etapa de reencontrame conmigo mismo hasta que volvi a ser yo')
+}
